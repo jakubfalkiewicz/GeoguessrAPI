55422ac409031a0d1382ae1cc5b19e48f3bc9b76
Jakub Falkiewicz <kufal222@gmail.com>
2023-04-09 20:54:42 +0200

    add exponent prop

--- models/Map.js
+++ models/Map.js
@@ -11,6 +11,7 @@ const mapSchema = new Schema({
       lng: Number,
     },
   ],
+  exponent: { type: Number, required: true },
   zoomLevel: Number,
   country: { type: String, required: false },
   createDate: { type: Date, default: Date.now },
